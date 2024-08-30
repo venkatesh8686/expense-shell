9435fb8eb21176de17734d9f78b927a9ff03556a
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-08-30 22:01:56 +0530

    this again one step forward

--- mysql.sh
+++ mysql.sh
@@ -46,11 +46,11 @@ VALIDATION $? "mysqld is started working"
 # systemctl status mysqld
 # VALIDATION $? "Status of mysqlsd"&>>$LOG_FILES 
 
-mysql -h mysql.vvsmgold.online -u root -pExpenseApp@1 -e "show database;"&>>$LOG_FILES 
+mysql -h mysql.vvsmgold.online -u root -pExpenseApp@1 -e "show database;" &>>$LOG_FILES 
 if [ $? -ne 0 ]
 then
-    echo "Mysql root password settingnow"
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILES
+    echo "Mysql root password settingnow" &>>$LOG_FILES
+    mysql_secure_installation --set-root-pass ExpenseApp@1 
     VALIDATION $? "setting up root password"
 else
         echo -e "$Y mysql root password already setup..$Y skipping" | tee -a $LOG_FILES
